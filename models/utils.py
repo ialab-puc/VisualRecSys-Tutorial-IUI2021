ebda755ad3166f9bc3563e401856bd6a87a5a3af
Antonio Ossa <aaossa@uc.cl>
2021-02-27 20:33:56 -0300

    :sparkles: Add utils to manage models easily
    A method creates a copy of a model in CPU, while the other method can
    save a checkpoint of a given model with additional data
    
    Signed-off-by: Antonio Ossa <aaossa@uc.cl>

--- models/utils.py
+++ models/utils.py
@@ -1,5 +1,14 @@
+from collections import OrderedDict
+
 import torch
 import torchvision.models as models
+
+
+def get_cpu_copy(model):
+    return OrderedDict({
+        k: v.to("cpu")
+        for k, v in model.state_dict().items()
+    })
 
 
 def get_model_by_name(model_name, pretrained=True):
@@ -8,3 +17,13 @@ def get_model_by_name(model_name, pretrained=True):
     for param in model.parameters():
         model.requires_grad = False
     return model
+
+
+def save_checkpoint(checkpoint_path, **components):
+    checkpoint_dict = dict()
+    for name, component in components.items():
+        if hasattr(component, "state_dict"):
+            checkpoint_dict[name] = component.state_dict()
+        else:
+            checkpoint_dict[name] = component
+    torch.save(checkpoint_dict, checkpoint_path)
