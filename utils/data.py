34a43e7e1d45624faeaa10e4b21f8ff54733d801
isipalma <impalma@uc.cl>
2021-06-15 11:17:58 -0400

    positive item sampling and fix infinite loop

--- utils/data.py
+++ utils/data.py
@@ -34,3 +34,24 @@ def get_interactions_dataframe(interactions_path, display_stats=False):
             print(f"Interactions - {column}: {interactions_df[column].nunique()} unique values")
 
     return interactions_df
+
+def mark_evaluation_rows(interactions_df, threshold=None):
+    if threshold is None:
+        threshold = 1
+
+    def _mark_evaluation_rows(group):
+        # Only the last 'threshold' items are used for evaluation,
+        # unless less items are available (then they're used for training)
+        evaluation_series = pd.Series(False, index=group.index)
+        if len(group) > threshold:
+            evaluation_series.iloc[-threshold:] = True
+        return evaluation_series
+
+    # Mark evaluation rows
+    interactions_df["evaluation"] = interactions_df.groupby(
+        ["user_id"])["user_id"].apply(_mark_evaluation_rows)
+    # Sort transactions by timestamp
+    interactions_df = interactions_df.sort_values("timestamp")
+    # Reset index according to new order
+    interactions_df = interactions_df.reset_index(drop=True)
+    return interactions_df
